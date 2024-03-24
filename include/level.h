4e8b1ff0d29d69910edad99896d01e5d4226c19f
Tyler <lorenarcdev@gmail.com>
2024-03-24 11:28:49 -0400

    Defined some preprocesser macros, and declared some important member functions and variables.

--- include/level.h
+++ include/level.h
@@ -1,12 +1,25 @@
 // level.h
 #pragma once
 #include <raylib.h>
+#include <array>
+
+using std::array;
+
+#define MAP_WIDTH 7
+#define MAP_HEIGHT 7
+#define TOTAL_TILES (MAP_WIDTH * MAP_HEIGHT)
 
 
 class Level {
 public:
   Level();
   ~Level();
+
+  array<array<int, MAP_WIDTH>, MAP_HEIGHT> level_grid;
+
+  void loadLevelGrid();
+  void printLevelGrid();
+  array<int, TOTAL_TILES> getRawGrid();
 
   void setupModel();
   void draw();
